29e7ba980eb357c41d448c37a9dfc704f8a3221e
YJM <jmyoon4488@gmail.com>
2021-11-14 01:09:10 +0900

    [modify] 3. Modifiers

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jmyoon4488/basiccodelabweek2/ui/theme/Theme.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/jmyoon4488/basiccodelabweek2/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun BasicCodeLabWeek2Theme(
+fun LayoutsCodelabTheme (
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
