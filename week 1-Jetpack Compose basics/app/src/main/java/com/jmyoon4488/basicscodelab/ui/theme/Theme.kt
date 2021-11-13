ad6f497506ed8eb530ea7eb88790997f6145c8b7
YJM <jmyoon4488@gmail.com>
2021-11-14 00:48:13 +0900

    [modify] 12. Styling and theming your app

--- week 1-Jetpack Compose basics/app/src/main/java/com/jmyoon4488/basicscodelab/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/app/src/main/java/com/jmyoon4488/basicscodelab/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
         primary = Purple200,
@@ -12,19 +13,26 @@ private val DarkColorPalette = darkColors(
         secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+//private val LightColorPalette = lightColors(
+//        primary = Purple500,
+//        primaryVariant = Purple700,
+//        secondary = Teal200
+//
+//        /* Other default colors to override
+//    background = Color.White,
+//    surface = Color.White,
+//    onPrimary = Color.White,
+//    onSecondary = Color.Black,
+//    onBackground = Color.Black,
+//    onSurface = Color.Black,
+//    */
+//)
 
-        /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
 
 @Composable
